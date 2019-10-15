9e168893eb1e249d375a11094da47d3d982b9b2f
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-15 08:45:05 +0000

    RMX1911: oppo_project: hardcode for OPPO_19631
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: I305bfe704a8f8136b6d62b83ccf790e2da444dab
    Signed-off-by: dodyirawan85 <johntheryjt@gmail.com>

--- drivers/soc/oppo/oppo_project/oppo_project.c
+++ drivers/soc/oppo/oppo_project/oppo_project.c
@@ -41,14 +41,6 @@ unsigned int get_project(void)
         return format->nproject;
 }
 
-unsigned int is_project(OPPO_PROJECT project)
-{
-        return (get_project() == project?1:0);
-}
-#ifdef CONFIG_PRODUCT_REALME
-EXPORT_SYMBOL(is_project);
-#endif /* CONFIG_PRODUCT_REALME */
-
 unsigned char get_PCB_Version(void)
 {
         if (format == NULL) {
